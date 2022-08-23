2e81cbd3e843e5d9beef7baf7f435a12a2ab9477
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-08-23 14:14:37 +0300

    HomeWork7

---
+++ HomeWork7/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int granny;
+        int interval = 10;
+        int totalTime;
+        int minutes;
+        int hour;
+
+        Console.Write("Введите количество старушек в очереди: ");
+        granny = Convert.ToInt32(Console.ReadLine());
+
+        totalTime = granny * interval;
+        hour = totalTime / 60;
+        minutes = totalTime % 60;
+
+        Console.WriteLine($"Вам придётся отстоять в очереди {hour} часов и {minutes} минут");
+    }
+}
