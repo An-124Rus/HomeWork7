d569543f3bc4fca420bdc3b38144737d9090bbfa
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-08-23 15:27:50 +0300

    name of integers and constant fixed

--- HomeWork7/Program.cs
+++ HomeWork7/Program.cs
@@ -2,19 +2,22 @@
 {
     private static void Main(string[] args)
     {
+        int hour;
+        int minute = 1;        
         int numberOfGrannies;
         int interval = 10;
         int totalTime;
-        int remainHours;
-        int remainMinutes;        
+        int hoursInQueue;
+        int minutesInQueue;        
 
         Console.Write("Введите количество старушек в очереди: ");
         numberOfGrannies = Convert.ToInt32(Console.ReadLine());
 
+        hour = minute * 60;
         totalTime = numberOfGrannies * interval;
-        remainHours = totalTime / 60;
-        remainMinutes = totalTime % 60;
+        hoursInQueue = totalTime / hour;
+        minutesInQueue = totalTime % hour;
 
-        Console.WriteLine($"Вам придётся отстоять в очереди {remainHours} часов и {remainMinutes} минут");
+        Console.WriteLine($"Вам придётся отстоять в очереди {hoursInQueue} часов и {minutesInQueue} минут");
     }
 }
